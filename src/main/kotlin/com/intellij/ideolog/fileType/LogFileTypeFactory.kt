a7ac53c460706fcdec99ae6420ba818dfa61cf34
Dmitry Neverov <dmitry.neverov@gmail.com>
2017-11-22 08:23:58 +0100

    Associate ideolog with rotated logs
    They are often have extensions like xxx.log.1.

--- src/main/kotlin/com/intellij/ideolog/fileType/LogFileTypeFactory.kt
+++ src/main/kotlin/com/intellij/ideolog/fileType/LogFileTypeFactory.kt
@@ -1,5 +1,6 @@
 package com.intellij.ideolog.fileType
 
+import com.intellij.openapi.fileTypes.FileNameMatcher
 import com.intellij.openapi.fileTypes.FileTypeConsumer
 import com.intellij.openapi.fileTypes.FileTypeFactory
 
@@ -7,6 +8,14 @@ class LogFileTypeFactory : FileTypeFactory() {
   private val LOG_FILE_EXTENSIONS = "log" // add more extensions separated with ;
 
   override fun createFileTypes(consumer: FileTypeConsumer) {
-    consumer.consume(LogFileType, LOG_FILE_EXTENSIONS)
+    LOG_FILE_EXTENSIONS.split(";").forEach {
+      consumer.consume(LogFileType, object : FileNameMatcher {
+        private val pattern = Regex("^.*\\.$it(.\\d+)?$")
+
+        override fun accept(fileName: String): Boolean = pattern.matches(fileName)
+
+        override fun getPresentableString(): String = it
+      })
+    }
   }
 }
