de0d23398aef0b4e83354f11eabb658c6d403ff3
Nikolay Kuznetsov <kna.rus@gmail.com>
2019-12-04 18:54:17 +0100

    Properly update action availability to avoid hotkey clashes

--- src/main/kotlin/com/intellij/ideolog/editorActions/GoToNextEntryAction.kt
+++ src/main/kotlin/com/intellij/ideolog/editorActions/GoToNextEntryAction.kt
@@ -8,27 +8,35 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 
 class GoToNextEntryAction : AnAction() {
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabled = canExecute(e)
+  }
+
+  private fun canExecute(e: AnActionEvent): Boolean {
+    val psiFile = e.dataContext.getData(CommonDataKeys.PSI_FILE) ?: return false
+
+    return psiFile.fileType == LogFileType
+  }
+
   override fun actionPerformed(e: AnActionEvent) {
     val editor = e.dataContext.getData(CommonDataKeys.EDITOR) ?: return
     val psiFile = e.dataContext.getData(CommonDataKeys.PSI_FILE) ?: return
     val project = e.dataContext.getData(CommonDataKeys.PROJECT) ?: return
-    val enabled = psiFile.fileType == LogFileType
-    e.presentation.isEnabled = enabled
-    if (enabled) {
-      val foldingModel = editor.foldingModel
-      var event = LogEvent.fromEditor(editor, editor.caretModel.offset)
-      var nextPos = event.endOffset + 1
-      while (nextPos < editor.document.textLength) {
-        if (foldingModel.isOffsetCollapsed(nextPos)) {
-          event = LogEvent.fromEditor(editor, nextPos)
-          nextPos = event.endOffset + 1
-        } else {
-          event = LogEvent.fromEditor(editor, nextPos)
-          val descriptor = OpenFileDescriptor(project, psiFile.virtualFile, event.startOffset)
-          val navigable = descriptor.setUseCurrentWindow(true)
-          if (navigable.canNavigate()) navigable.navigate(true)
-          return
-        }
+    if (!canExecute(e)) return
+
+    val foldingModel = editor.foldingModel
+    var event = LogEvent.fromEditor(editor, editor.caretModel.offset)
+    var nextPos = event.endOffset + 1
+    while (nextPos < editor.document.textLength) {
+      if (foldingModel.isOffsetCollapsed(nextPos)) {
+        event = LogEvent.fromEditor(editor, nextPos)
+        nextPos = event.endOffset + 1
+      } else {
+        event = LogEvent.fromEditor(editor, nextPos)
+        val descriptor = OpenFileDescriptor(project, psiFile.virtualFile, event.startOffset)
+        val navigable = descriptor.setUseCurrentWindow(true)
+        if (navigable.canNavigate()) navigable.navigate(true)
+        return
       }
     }
   }
