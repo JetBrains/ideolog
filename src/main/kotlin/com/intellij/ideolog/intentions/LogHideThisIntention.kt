86d80c2d269ca1299d06bac5197f3c6abdda63e9
Mikhail Filippov <mikhail@filippov.me>
2020-02-12 14:22:17 +0300

    Fix mistake when remove duplication.

--- src/main/kotlin/com/intellij/ideolog/intentions/LogHideThisIntention.kt
+++ src/main/kotlin/com/intellij/ideolog/intentions/LogHideThisIntention.kt
@@ -1,7 +1,13 @@
 package com.intellij.ideolog.intentions
 
+import com.intellij.ideolog.util.ideologContext
+import com.intellij.openapi.editor.Editor
+
 class LogHideThisIntention : LogThisIntentionBase() {
   override fun getText(): String {
     return "Hide lines with '$fieldText' in this field"
   }
+
+  override fun getIntentionItems(editor: Editor): HashSet<Pair<Int, String>> =
+    editor.document.ideologContext.hiddenItems
 }
