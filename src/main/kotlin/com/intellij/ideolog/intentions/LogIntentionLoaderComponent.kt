fe8c0c68d33136799c36f71adb1741a8488f6d70
Mikhail Filippov <mikhail@filippov.me>
2020-02-28 13:27:28 +0300

    Remove component which block plugin loading without restart.

--- src/main/kotlin/com/intellij/ideolog/intentions/LogIntentionLoaderComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.intellij.ideolog.intentions
-
-import com.intellij.codeInsight.intention.IntentionManager
-
-class LogIntentionLoaderComponent {
-  init {
-    IntentionManager.getInstance()
-  }
-}
