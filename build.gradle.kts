daa9945a7a2c0c232955ce3c8aebb3192920db3d
Mikhail Filippov <mikhail@filippov.me>
2020-04-01 10:00:12 +0300

    Cleanup build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-  // TODO: Switch to after 1.3.70 released
-  val kotlinVersion = "1.3.70-eap-184"
+  val kotlinVersion = "1.3.70"
 
   repositories {
     mavenCentral()
-    // TODO: Remove after 1.3.70 released
-    maven("https://dl.bintray.com/kotlin/kotlin-eap")
   }
+
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
   }
@@ -18,7 +16,12 @@ plugins {
   id("me.filippov.gradle.jvm.wrapper") version "0.9.3"
   id("org.jetbrains.intellij") version "0.4.16"
 }
+
 apply(plugin = "kotlin")
+
+repositories {
+  mavenCentral()
+}
 
 group = "ideolog"
 val buildNumber: String by rootProject.extra
@@ -33,12 +36,6 @@ intellij {
       setUntilBuild("")
     }
   }
-}
-
-repositories {
-  mavenCentral()
-  // TODO: Remove after 1.3.70 released
-  maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 tasks.withType<KotlinCompile> {
