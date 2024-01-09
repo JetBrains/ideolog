a8d34e9bbcb477ae954887c50fa581684241b02f
ArtemKar123 <wartemtem@yandex.ru>
2024-01-09 16:35:38 +0100

    Add common stacktrace filter

---
+++ src/main/kotlin/com/intellij/ideolog/filters/StackTraceFileFilter.kt
@@ -0,0 +1,98 @@
+package com.intellij.ideolog.filters
+
+import com.intellij.execution.filters.Filter
+import com.intellij.execution.filters.HyperlinkInfo
+import com.intellij.execution.filters.LazyFileHyperlinkInfo
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.LocalFileSystem
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+open class StackTraceFileFilter(
+  private val project: Project,
+  private val localFileSystem: LocalFileSystem
+) : Filter, DumbAware {
+  companion object {
+    private const val FILE_LINE_REGEX = """(\(\d+\)|:\d+)"""
+    private const val LINUX_MACOS_PATH_REGEX = """(/(?:[\w.-]+/)*\w+\.\w+)"""
+    private const val WINDOWS_PATH_REGEX = """\b((?:[a-zA-Z]:\\(?:[^\\/:*?"<>|\r\n]+\\)*)?[^\\/:*?"<>|\r\n]*)"""
+    private val LINUX_MACOS_FILE_PATTERN =
+      Pattern.compile("""$LINUX_MACOS_PATH_REGEX$FILE_LINE_REGEX""")
+    private val WINDOWS_FILE_PATTERN =
+      Pattern.compile("""$WINDOWS_PATH_REGEX$FILE_LINE_REGEX""")
+
+    private fun canContainFilePathFromLaravelLogs(line: String): Boolean {
+      return (line.contains(":\\") || line.contains('/')) &&
+        ((line.contains('(') && line.contains(')')) || line.contains(':'))
+    }
+  }
+
+  override fun applyFilter(line: String, entireLength: Int): Filter.Result? {
+    if (!canContainFilePathFromLaravelLogs(line)) return null
+
+    val textStartOffset = entireLength - line.length
+    val bombedCharSequence = StringUtil.newBombedCharSequence(line, 100)
+    val filterResultItems =
+      filterNestedResults(
+        collectFilterResultItems(textStartOffset, LINUX_MACOS_FILE_PATTERN.matcher(bombedCharSequence)) +
+          collectFilterResultItems(textStartOffset, WINDOWS_FILE_PATTERN.matcher(bombedCharSequence))
+      )
+
+    return when (filterResultItems.size) {
+      0 -> null
+      1 -> filterResultItems.first().let {
+        Filter.Result(
+          it.highlightStartOffset,
+          it.highlightEndOffset,
+          it.hyperlinkInfo
+        )
+      }
+
+      else -> Filter.Result(filterResultItems)
+    }
+  }
+
+  private fun filterNestedResults(results: List<Filter.ResultItem>): List<Filter.ResultItem> =
+    results
+      .sortedBy { it.highlightStartOffset }
+      .fold(mutableListOf<Pair<Int, Filter.ResultItem>>()) { acc, result ->
+        if ((acc.lastOrNull()?.first ?: -1) < result.highlightStartOffset) {
+          acc.add(result.highlightEndOffset to result)
+        }
+        acc
+      }
+      .map { it.second }
+
+  private fun collectFilterResultItems(textStartOffset: Int, matcher: Matcher): List<Filter.ResultItem> {
+    val resultItems = mutableListOf<Filter.ResultItem>()
+    while (matcher.find()) {
+      resultItems.add(
+        Filter.ResultItem(
+          textStartOffset + matcher.start(),
+          textStartOffset + matcher.end(),
+          buildFileHyperlinkInfo(matcher.group(1), matcher.takeIf { it.groupCount() >= 2 }?.group(2))
+        )
+      )
+    }
+    return resultItems
+  }
+
+  private fun buildFileHyperlinkInfo(filePath: String, documentLineString: String?): HyperlinkInfo? =
+    localFileSystem.findFileByPath(filePath)?.let {
+      val lineNumber = documentLineString
+        ?.filter { it.isDigit() }
+        ?.takeIf { it.isNotEmpty() }
+        ?.toInt()
+        ?.let { it - 1 } ?: 0
+
+      LinedFileHyperlinkInfo(project, filePath, lineNumber)
+    }
+
+  class LinedFileHyperlinkInfo(
+    project: Project,
+    val filePath: String,
+    val documentLine: Int,
+  ) : LazyFileHyperlinkInfo(project, filePath, documentLine, 0, false)
+}
---
+++ src/test/kotlin/com/intellij/ideolog/filters/StackTraceFileFilterTest.kt
@@ -0,0 +1,125 @@
+package com.intellij.ideolog.filters
+
+import com.intellij.execution.filters.Filter
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.testFramework.RunsInEdt
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import junit.framework.TestCase
+import org.junit.rules.TemporaryFolder
+
+@RunsInEdt
+internal class StackTraceFileFilterTests : BasePlatformTestCase() {
+
+  private lateinit var filter: StackTraceFileFilter
+  private val tmpFolder: TemporaryFolder = TemporaryFolder()
+
+  override fun setUp() {
+    super.setUp()
+    tmpFolder.create()
+    filter = StackTraceFileFilter(project, LocalFileSystem.getInstance())
+  }
+
+  override fun tearDown() {
+    super.tearDown()
+    tmpFolder.delete()
+  }
+
+  fun `test no file hyperlink`() {
+    assertNoFileHyperlink("")
+    assertNoFileHyperlink("No file hyperlink")
+    assertNoFileHyperlink("""/Users\me/Application.php""")
+    assertNoFileHyperlink("""C:\Users\me/Application.php""")
+  }
+
+  fun `test single file hyperlink`() {
+    assertFileHyperlink("hi /Application.php(35)", 3, 23, "/Application.php", 35, false)
+    assertFileHyperlink("/Application.php:35", 0, 19, "/Application.php", 35, false)
+    assertFileHyperlink("/Users/me/Application.php(35)", 0, 29, "/Users/me/Application.php", 35, false)
+    assertFileHyperlink("/Users/me/Application.php:35", 0, 28, "/Users/me/Application.php", 35, false)
+    assertFileHyperlink("""C:\Users\me\Application.php(34)""", 0, 31, """C:\Users\me\Application.php""", 34, false)
+    assertFileHyperlink("""C:\Users\me\Application.php:34""", 0, 30, """C:\Users\me\Application.php""", 34, false)
+  }
+
+  fun `test multiple logs file hyperlinks`() {
+    assertFileHyperlinks(
+      applyFilter("{ #stacktrace: /Users/me/Application.php(35) /Users/me/Kernel.php:42 }"),
+      listOf(
+        FileLinkInfo(15, 44, "/Users/me/Application.php", 35, false),
+        FileLinkInfo(45, 68, "/Users/me/Kernel.php", 42, false)
+      )
+    )
+  }
+
+  fun `test apply Filter to existing file path on linux or mac`() {
+    val existingFile = tmpFolder.newFile("Application.php")
+    val filePathLength = existingFile.absolutePath.length
+    assertFileHyperlink("#0 ${existingFile.absolutePath}(2)", 3, 6 + filePathLength, existingFile.absolutePath, 2, true)
+  }
+
+  fun `test apply Filter to multiple existing files path on linux or mac`() {
+    val firstExistingFile = tmpFolder.newFile("Application.php")
+    val firstFilePathLength = firstExistingFile.absolutePath.length
+    val secondExistingFile = tmpFolder.newFile("Kernel.php")
+    val secondFilePathLength = secondExistingFile.absolutePath.length
+    assertFileHyperlinks(
+      applyFilter("#0 ${firstExistingFile.absolutePath}(2), ${secondExistingFile.absolutePath}(4)"),
+      listOf(
+        FileLinkInfo(3, 6 + firstFilePathLength, firstExistingFile.absolutePath, 2, true),
+        FileLinkInfo(8 + firstFilePathLength, 11 + firstFilePathLength + secondFilePathLength, secondExistingFile.absolutePath, 4, true)
+      )
+    )
+  }
+
+  private fun applyFilter(line: String) = filter.applyFilter(line, line.length)
+
+  private fun assertNoFileHyperlink(text: String) {
+    assertNull(applyFilter(text))
+  }
+
+  private fun assertFileHyperlink(
+    text: String,
+    highlightStartOffset: Int,
+    highlightEndOffset: Int,
+    filePath: String,
+    documentLine: Int,
+    isFileExists: Boolean
+  ) {
+    assertFileHyperlinks(
+      applyFilter(text),
+      listOf(FileLinkInfo(highlightStartOffset, highlightEndOffset, filePath, documentLine, isFileExists))
+    )
+  }
+
+  private fun assertFileHyperlinks(result: Filter.Result?, infos: List<FileLinkInfo>) {
+    assertNotNull(result)
+    result?.let {
+      val items = result.resultItems
+      assertEquals(infos.size, items.size)
+      infos.indices.forEach { assertHyperlink(items[it], infos[it]) }
+    }
+  }
+
+  private fun assertHyperlink(actualItem: Filter.ResultItem, expectedFileLinkInfo: FileLinkInfo) {
+    assertEquals(expectedFileLinkInfo.highlightStartOffset, actualItem.highlightStartOffset)
+    assertEquals(expectedFileLinkInfo.highlightEndOffset, actualItem.highlightEndOffset)
+    if (expectedFileLinkInfo.isFileExists) {
+      assertInstanceOf(actualItem.hyperlinkInfo, StackTraceFileFilter.LinedFileHyperlinkInfo::class.java)
+      assertFileLink(expectedFileLinkInfo, actualItem.hyperlinkInfo as StackTraceFileFilter.LinedFileHyperlinkInfo)
+    } else {
+      TestCase.assertNull(actualItem.hyperlinkInfo)
+    }
+  }
+
+  private fun assertFileLink(expected: FileLinkInfo, actual: StackTraceFileFilter.LinedFileHyperlinkInfo) {
+    assertEquals(expected.filePath, actual.filePath)
+    assertEquals(expected.line, actual.documentLine + 1)
+  }
+
+  data class FileLinkInfo(
+    val highlightStartOffset: Int,
+    val highlightEndOffset: Int,
+    val filePath: String,
+    val line: Int,
+    val isFileExists: Boolean
+  )
+}
