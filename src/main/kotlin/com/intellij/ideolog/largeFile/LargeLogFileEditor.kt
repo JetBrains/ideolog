460b33915987876849c6150f2af2553731197ded
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-06-28 12:37:10 +0200

    WI-78024 Provide separate editor type id for LargeLogFileEditorProvider
    (cherry picked from commit 240b470ca244775f77217b50b326e304613bd437)
    
    IJ-CR-138762
    
    GitOrigin-RevId: e9a61a8e1ddfb985bbfb3bb70de9acaa91deed4a

--- src/main/kotlin/com/intellij/ideolog/largeFile/LargeLogFileEditor.kt
+++ src/main/kotlin/com/intellij/ideolog/largeFile/LargeLogFileEditor.kt
@@ -1,7 +1,10 @@
 package com.intellij.ideolog.largeFile
 
 import com.intellij.ideolog.IdeologBundle
+import com.intellij.ideolog.largeFile.fileType.LargeLogFileType
 import com.intellij.largeFilesEditor.editor.LargeFileEditor
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory
 
 class LargeLogFileEditor(delegate: LargeFileEditor) : LargeFileEditor by delegate {
   override fun getName() = IdeologBundle.message("large.log")
@@ -9,5 +12,8 @@ class LargeLogFileEditor(delegate: LargeFileEditor) : LargeFileEditor by delegat
   init {
     editor.settings.isUseSoftWraps = false
     this.editor.putUserData(LargeFileEditor.LARGE_FILE_EDITOR_SOFT_WRAP_KEY, false)
+    val scheme = EditorColorsManager.getInstance().globalScheme
+    val editorHighlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(LargeLogFileType, scheme, project)
+    this.trySetHighlighter(editorHighlighter)
   }
 }
