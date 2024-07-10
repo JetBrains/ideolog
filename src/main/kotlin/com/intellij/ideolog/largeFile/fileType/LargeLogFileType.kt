5dfa1e14b512411448a829e1ea6ffc03331c9ebd
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-07-10 15:52:45 +0200

    WI-78195 Make LargeLogFileType to be a secondary file type for .log files
    GitOrigin-RevId: 3fff8287d71fa1f6e623ed17aaf786db3346a44e

--- src/main/kotlin/com/intellij/ideolog/largeFile/fileType/LargeLogFileType.kt
+++ src/main/kotlin/com/intellij/ideolog/largeFile/fileType/LargeLogFileType.kt
@@ -3,7 +3,7 @@ package com.intellij.ideolog.largeFile.fileType
 import com.intellij.ideolog.IdeologBundle
 import com.intellij.ideolog.file.LogIcons
 
-object LargeLogFileType : com.intellij.openapi.fileTypes.LanguageFileType(LargeLogLanguage) {
+object LargeLogFileType : com.intellij.openapi.fileTypes.LanguageFileType(LargeLogLanguage, true) {
   override fun getName(): String = "LargeLog"
   override fun getDescription(): String = IdeologBundle.message("label.large.log.files")
   override fun getDefaultExtension(): String = "log"
