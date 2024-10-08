dbc07d6f1cb12eb847f7f5f46cb0543caf537d29
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-10-08 18:26:54 +0200

    WI-78313 Allow date and time highlighting as a token
    (cherry picked from commit 3f159fe4784368dd1b078f17f3a68f5cefc6d834)
    
    IJ-CR-147508
    
    GitOrigin-RevId: b1b04ba159838d78a85cdefbd1e771c152c29a3a

---
+++ src/main/kotlin/com/intellij/ideolog/highlighting/LogColors.kt
@@ -0,0 +1,9 @@
+package com.intellij.ideolog.highlighting
+
+import com.intellij.ui.JBColor
+import java.awt.Color
+
+internal val DATE_COLOR = JBColor(
+  Color(0, 160, 160),
+  Color(0, 138, 138),
+)
