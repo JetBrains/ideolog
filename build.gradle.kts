246f7e163033d9930400476ffd29834628121742
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-10-19 10:45:24 +0200

    IJPL-213640 update kotlinx serialization to 1.9.0
    GitOrigin-RevId: 05927722ce98708d58d505f720323b5f327cba9a

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
 
   testImplementation("junit:junit:4.13.2")
 
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.8.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.9.0")
 }
 
 intellijPlatform {
